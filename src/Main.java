93fe0e7ea1df6f9beb154928145a9b5d35d419d0
이현진 <roje2003@naver.com>
2023-03-27 17:34:03 +0900

    예외처리

--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,19 @@
-import java.time.LocalDate;
+import java.util.InputMismatchException;
 import java.util.Scanner;
-class Friend{
-    protected String name;
-    protected String phone;
 
-    public Friend(String na, String ph){
-        name = na;
-        phone = ph;
-    }
-    public void showInfo(){
-        System.out.println("이름: " + name);
-        System.out.println("전화: " + phone);
-    }
-}
-class ComFriend extends Friend{
-    private String department;
-
-    public ComFriend(String na, String de, String ph){
-        super(na, ph);
-        department = de;
-    }
-    public void showInfo(){
-        super.showInfo();
-        System.out.println("부서: " + department);
+class ExceptionCase{
+    public static void main(String[] args){
+        Scanner kb = new Scanner(System.in);
+        try{
+            System.out.print("a/b...a? ");
+            int n1 = kb.nextInt();
+            System.out.print("a/b...b? ");
+            int n2 =kb.nextInt();
+            System.out.printf("%d / %d = %d \n", n1, n2, n1/n2);
+        }
+        catch(ArithmeticException | InputMismatchException e){
+            e.getMessage();
+        }
+        System.out.println("Good bye~~!");
     }
 }
