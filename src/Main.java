78dd4d9458774bf7627dc24e87f7364a7211dbf2
이현진 <roje2003@naver.com>
2023-03-29 16:13:13 +0900

    인스턴스의 비교Equals

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,51 @@
-import java.util.InputMismatchException;
-import java.util.Scanner;
+class Point {
+    private int xPos;
+    private int yPos;
+    public Point(int x,int y){
+        xPos = x;
+        yPos = y;
+    }
+    public boolean equals(Object obj){
+        Point p = (Point)obj;
 
-class ExceptionCase{
-    public static void main(String[] args){
-        Scanner kb = new Scanner(System.in);
-        try{
-            System.out.print("a/b...a? ");
-            int n1 = kb.nextInt();
-            System.out.print("a/b...b? ");
-            int n2 =kb.nextInt();
-            System.out.printf("%d / %d = %d \n", n1, n2, n1/n2);
-        }
-        catch(ArithmeticException | InputMismatchException e){
-            e.getMessage();
-        }
-        System.out.println("Good bye~~!");
+        if(xPos == p.xPos && yPos == p.yPos)
+            return true;
+        else
+            return false;
     }
 }
+class Rectangle{
+    private Point upperLeft; //좌측 상단 좌표
+    private Point lowerRight; // 우측 하단 좌표
+    public Rectangle(int x1, int y1, int x2, int y2){
+        upperLeft = new Point(x1, y1);
+        lowerRight = new Point(x2, y2);
+    }
+    public boolean equals(Object obj){
+        Rectangle r = (Rectangle)obj;
+        if(upperLeft.equals(r.upperLeft) && lowerRight.equals(r.lowerRight))
+            return true;
+        else
+            return false;
+    }
+}
+class RectangleEquals{
+    public static void main(String[] arg){
+        Point p1 = new Point(1,5);
+        Point p2 = new Point(1,5);
+
+        Rectangle r1 = new Rectangle(1,6,2,7);
+        Rectangle r2 = new Rectangle(1,6,2,7);
+
+        if(p1.equals(p2))
+            System.out.println("같다");
+        else
+            System.out.println("다르다");
+
+        if(r1.equals(r2))
+            System.out.println("같다");
+        else
+            System.out.println("다르다");
+    }
+}
+
