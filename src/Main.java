4d39538c5b662b56b27c888dc1b417c93b424233
이현진 <roje2003@naver.com>
2023-03-30 17:42:38 +0900

    ArrayObjSort

--- src/Main.java
+++ src/Main.java
@@ -1,28 +1,29 @@
 import java.util.Arrays;
-
-class INum{
-    private int num;
-    public INum(int num){
-        this.num = num;
+class Person implements Comparable{
+    private String name;
+    private int age;
+    public Person(String name, int age){
+        this.name = name;
+        this.age = age;
     }
-    @Override  // 없으면 false를 반환 왜?? 인스턴스의 equals비교는 기본적으로 참조값 비교를 하지만 Override를 해줌으로써 값 비교로 바꾸기 때문
-    public boolean equals(Object obj){
-        if(this.num == ((INum)obj).num)  // 두 인스턴스의 내용 비교
-            return true;
-        else
-            return false;
+    @Override
+    public int compareTo(Object o){
+        Person p = (Person)o;
+        return this.age - p.age;
+    }
+    @Override
+    public String toString(){
+        return name + ": " + age;
     }
 }
-class ArrayObjEquals{
+class ArrayObjSort{
     public static void main(String[] args){
-        INum[] ar1 = new INum[3];
-        INum[] ar2 = new INum[3];
-        ar1[0] = new INum(1); ar2[0] = new INum(1);
-        ar1[1] = new INum(2); ar2[1] = new INum(2);
-        ar1[2] = new INum(3); ar2[2] = new INum(3);
-        System.out.println(Arrays.equals(ar1, ar2));
+        Person[] ar = new Person[3];
+        ar[0] = new Person("Hyun", 28);
+        ar[1] = new Person("Tae", 25);
+        ar[2] = new Person("Jun", 45);
+        Arrays.sort(ar);
+        for(Person p : ar)
+            System.out.println(p);
     }
 }
-
-
-
