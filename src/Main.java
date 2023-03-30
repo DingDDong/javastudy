4364171ffa73c375aeb34995697f5c8fb102fc22
이현진 <roje2003@naver.com>
2023-03-30 14:09:26 +0900

    난수 생성

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,15 @@
-class WrapperClassMethod {
-    public static void main(String[] args) {
-        //클래스 메소드를 통한 인스턴스 생성 방법 두 가지
-        Integer n1 = Integer.valueOf(5); //숫자 기반 Integer인스턴스 생성
-        Integer n2 = Integer.valueOf("1024"); // 문자열 기반 Integer인스턴스 생성
-        //대소 비교와 합을 계산하는 클래스 메소드
-        System.out.println("큰 수: " + Integer.max(n1, n2));
-        System.out.println("작은 수: " + Integer.min(n1, n2));
-        System.out.println("합: " + Integer.sum(n1, n2));
-        System.out.println();
-        //정수에 대한 2진,8진,16진수 표현 결과를 반환하는 클래스
-        System.out.println("12의 2진 표현: " + Integer.toBinaryString(12));
-        System.out.println("12의 8진 표현: " + Integer.toOctalString(12));
-        System.out.println("12의 16진 표현: " + Integer.toHexString(12));
+import java.util.Random;
+
+class PseudoRandom{
+    public static void main(String[] args){
+        System.out.println("실행할 때 마다 같음");
+        Random rand = new Random(12);
+        for(int i = 0; i < 7; i++)
+            System.out.println(rand.nextInt(1000));
+        System.out.println("실행할 때 마다 다름");
+        Random rand1 = new Random();
+        for(int i = 0; i < 7; i++)
+            System.out.println(rand1.nextInt(1000));
     }
 }
 
