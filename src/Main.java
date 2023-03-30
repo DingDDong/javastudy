5172a2b1d841a856670410393e27517e67c88303
이현진 <roje2003@naver.com>
2023-03-30 11:17:09 +0900

    Number클래스의 추상메소드 호출

--- src/Main.java
+++ src/Main.java
@@ -1,51 +1,11 @@
-class Point {
-    private int xPos;
-    private int yPos;
-    public Point(int x,int y){
-        xPos = x;
-        yPos = y;
-    }
-    public boolean equals(Object obj){
-        Point p = (Point)obj;
-
-        if(xPos == p.xPos && yPos == p.yPos)
-            return true;
-        else
-            return false;
-    }
-}
-class Rectangle{
-    private Point upperLeft; //좌측 상단 좌표
-    private Point lowerRight; // 우측 하단 좌표
-    public Rectangle(int x1, int y1, int x2, int y2){
-        upperLeft = new Point(x1, y1);
-        lowerRight = new Point(x2, y2);
-    }
-    public boolean equals(Object obj){
-        Rectangle r = (Rectangle)obj;
-        if(upperLeft.equals(r.upperLeft) && lowerRight.equals(r.lowerRight))
-            return true;
-        else
-            return false;
-    }
-}
-class RectangleEquals{
-    public static void main(String[] arg){
-        Point p1 = new Point(1,5);
-        Point p2 = new Point(1,5);
-
-        Rectangle r1 = new Rectangle(1,6,2,7);
-        Rectangle r2 = new Rectangle(1,6,2,7);
-
-        if(p1.equals(p2))
-            System.out.println("같다");
-        else
-            System.out.println("다르다");
-
-        if(r1.equals(r2))
-            System.out.println("같다");
-        else
-            System.out.println("다르다");
+class NumberMethod{
+    public static void main(String[] args){
+        Integer num1 = new Integer (29);
+        System.out.println(num1.intValue());    //int형 값으로 반환
+        System.out.println(num1.doubleValue()); //double형 값으로 반환
+        Double num2 = new Double(3.14);
+        System.out.println(num2.doubleValue()); //double형 값으로 반환
+        System.out.println(num2.intValue());    //int형 값으로 반환
     }
 }
 
