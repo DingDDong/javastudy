19d547f46e67c29750c078d781a71f4067b2ebd5
이현진 <roje2003@naver.com>
2023-03-30 14:24:53 +0900

    StringTokenizer 문자열의 토큰 구분

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,17 @@
-import java.util.Random;
+import java.util.StringTokenizer;
 
-class PseudoRandom{
+class TokenizeString{
     public static void main(String[] args){
-        System.out.println("실행할 때 마다 같음");
-        Random rand = new Random(12);
-        for(int i = 0; i < 7; i++)
-            System.out.println(rand.nextInt(1000));
-        System.out.println("실행할 때 마다 다름");
-        Random rand1 = new Random();
-        for(int i = 0; i < 7; i++)
-            System.out.println(rand1.nextInt(1000));
+        StringTokenizer st1 = new StringTokenizer("PM:08:45",":");
+        while(st1.hasMoreTokens())
+            System.out.print(st1.nextToken() + ' ');
+        System.out.println();
+        StringTokenizer st2 = new StringTokenizer("12 + 36 - 8 / 2 = 44", "+-/= ");
+        while(st2.hasMoreTokens())
+            System.out.print(st2.nextToken() + ' ');
+        System.out.println();
     }
 }
+
+
 
