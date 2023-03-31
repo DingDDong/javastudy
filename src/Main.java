98c1e63ed64511e6785a89121c67eda5f4f6b8ea
이현진 <roje2003@naver.com>
2023-03-31 15:04:17 +0900

    제네릭클래스연습1

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,31 @@
-class Box<T>{
-    private T ob;
-    public void set(T o){
-        ob = o;
+class DBox<L, R>{
+    private L left;
+    private R right;
+
+    public void set(L o, R r) {
+        left = o;
+        right = r;
     }
-    public T get(){
-        return ob;
-    }
+    public String toString() { return left + " & " + right; }
 }
-class BoxinBox{
+class DDBox<U, D>{
+    private U left1;
+    private D right2;
+    public void set(U u, D d){
+        left1 = u;
+        right2 = d;
+    }
+    @Override
+    public String toString() { return left1.toString() + "\n" + right2.toString();}
+}
+class DDBoxDemo {
     public static void main(String[] args){
-        Box<String> sBox = new Box<>();
-        sBox.set("I am so happy.");
-        Box<Box<String>> tBox = new Box<>();
-        tBox.set(sBox);
-        Box<Box<Box<String>>> fBox = new Box<>();
-        fBox.set(tBox);
-        System.out.println(fBox.get().get().get());
+        DBox<String, Integer> box1 = new DBox<>();
+        box1.set("Apple", 25);
+        DBox<String, Integer> box2 = new DBox<>();
+        box2.set("Orange", 33);
+        DDBox<DBox<String, Integer>, DBox<String, Integer>> ddbox = new DDBox<>();
+        ddbox.set(box1, box2);
+        System.out.println(ddbox);
     }
 }
