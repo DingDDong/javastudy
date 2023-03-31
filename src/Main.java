9454ca79d9f154319534cbab946fe2b7ae53eb4e
이현진 <roje2003@naver.com>
2023-03-31 11:48:32 +0900

    제네릭 매개변수화 타입을 타입인자로 전달

--- src/Main.java
+++ src/Main.java
@@ -1,29 +1,20 @@
-import java.util.Arrays;
-class Person implements Comparable{
-    private String name;
-    private int age;
-    public Person(String name, int age){
-        this.name = name;
-        this.age = age;
+class Box<T>{
+    private T ob;
+    public void set(T o){
+        ob = o;
     }
-    @Override
-    public int compareTo(Object o){
-        Person p = (Person)o;
-        return this.age - p.age;
-    }
-    @Override
-    public String toString(){
-        return name + ": " + age;
+    public T get(){
+        return ob;
     }
 }
-class ArrayObjSort{
+class BoxinBox{
     public static void main(String[] args){
-        Person[] ar = new Person[3];
-        ar[0] = new Person("Hyun", 28);
-        ar[1] = new Person("Tae", 25);
-        ar[2] = new Person("Jun", 45);
-        Arrays.sort(ar);
-        for(Person p : ar)
-            System.out.println(p);
+        Box<String> sBox = new Box<>();
+        sBox.set("I am so happy.");
+        Box<Box<String>> tBox = new Box<>();
+        tBox.set(sBox);
+        Box<Box<Box<String>>> fBox = new Box<>();
+        fBox.set(tBox);
+        System.out.println(fBox.get().get().get());
     }
 }
