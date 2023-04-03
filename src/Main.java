b1a1159f79238e4d964c9d86c043a3e4fb07b04b
이현진 <roje2003@naver.com>
2023-04-03 17:18:25 +0900

    practice7-1생성자와클래스

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,19 @@
-class FirstJavaProgram {
+class Triangle {
+    final private double height;
+    final private double base;
+    public Triangle(double height,int base) {
+        this.height = height;
+        this.base = base;
+    }
+    public double getArea() {
+        return height * base / 2;
+    }
+}
+class useTriangle{
     public static void main(String[] args) {
-        for (int j = 1; j <= 1000; j++) {
-            if (isPrimeNumber(j))
-                System.out.println(j);
-        }
+        Triangle tr = new Triangle(5,3);
+        System.out.println(tr.getArea());
+        Triangle tr1 = new Triangle(10,8);
+        System.out.println(tr1.getArea());
     }
-        public static boolean isPrimeNumber ( int n){
-            if (n <= 1)
-                return false;
-            if (n == 2)
-                return true;
-            for (int i = 2; i < n; i++) {
-                if (n % i == 0)
-                    return false;
-            }
-            return true;
-        }
-    }
+}
