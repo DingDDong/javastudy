1a46b6c20fed819d4e2f28a655880fa90e5f6ec3
이현진 <roje2003@naver.com>
2023-04-03 15:16:37 +0900

    practice6-1-2 메소드

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,13 @@
 class FirstJavaProgram{
     public static void main(String[] args) {
-      result(10,5);
+      result(3,7);
     }
     public static void result(int x, int y){
-        System.out.println("더하기: " + (x + y));
-        System.out.println("빼기: " + (x - y));
-        System.out.println("곱하기: " + (x * y));
-        System.out.println("몫: " + (x / y));
-        System.out.println("나머지: " + (x % y));
+        if(x > y){
+            System.out.println("절대값: " + (x - y));
+        }
+        else{
+            System.out.println("절대값: " + (y - x));
+        }
     }
 }
