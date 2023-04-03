1db9ea95a9cf948cf2449eed536b197fc2e1d5de
이현진 <roje2003@naver.com>
2023-04-03 15:05:47 +0900

    practice5-3 switch

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,18 @@
 class FirstJavaProgram{
     public static void main(String[] args) {
-        int num1 = 50;
-        int num2 = 100;
-        int big;
-        int diff;
-
-        if(num1 > num2){
-            System.out.println("큰 수:" + num1);
+        int n = 24;
+        switch (n / 10){
+            case 0:
+                System.out.println("0이상 10미만의 수");
+                break;
+            case 1:
+                System.out.println("10이상 20미만의 수");
+                break;
+            case 2:
+                System.out.println("20이상 30미만의 수");
+                break;
+            default:
+                System.out.println("음수 혹은 30이상의 수");
         }
-        else
-            System.out.println("큰 수:" + num2);
-        if(num1 > num2)
-            System.out.println("절댓값:" + (num1 - num2));
-        else
-            System.out.println("절댓값:" + (num2 - num1));
     }
 }
