7446df8e06470b428484b39ea86ef9d923bba2af
이현진 <roje2003@naver.com>
2023-04-04 13:14:57 +0900

    practice13-2-1 Array

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,25 @@
 class Array{
-    public static int minValue(int[] arr){
-        int min = arr[0];
-        for(int i = 0; i < arr.length; i++){
-            if (min > arr[i]){
-                min = arr[i];
-            }
-        }
-        return min;
+    public static void addOneDArr(int[] arr, int add){
+        for(int i = 0; i < arr.length; i++)
+            arr[i] += add;
     }
-
-    public static int maxValue(int[] arr){
-        int max = arr[0];
-        for (int e : arr){
-            if(max < e){
-                max = e;
+    public static void addTwoDArr(int[][] arr,int add){
+        for(int i = 0; i < arr.length; i++)
+            addOneDArr(arr[i], add);
             }
-        }
-        return max;
-    }
     public static void main(String[] args) {
-        int[] arr = {234 , 10, 1, 2, 3, 4, 5, 6, 7};
-        System.out.println("최솟값: " + minValue(arr));
-        System.out.println("최댓값: " + maxValue(arr));
+        int[][] arr = {
+                {1, 2, 3, 4},
+                {5, 6, 7, 8},
+                {9, 10, 11, 12}
+        };
+
+        addTwoDArr(arr, 2);
+
+        for(int i = 0; i < arr.length; i++) {
+            for(int j = 0; j < arr[i].length; j++)
+                System.out.print(arr[i][j] + " ");
+            System.out.println();
+        }
     }
 }
