cf4e7ace556f51aeb23dcfd30c54b4477f41999a
이현진 <roje2003@naver.com>
2023-04-04 14:04:57 +0900

    practice13-2-2 Array

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,39 @@
 class Array{
-    public static void addOneDArr(int[] arr, int add){
-        for(int i = 0; i < arr.length; i++)
-            arr[i] += add;
-    }
-    public static void addTwoDArr(int[][] arr,int add){
-        for(int i = 0; i < arr.length; i++)
-            addOneDArr(arr[i], add);
+        public static void shiftTwoDArr(int[][] arr) {
+            int[] lastLow =  arr[arr.length - 1];
+
+            for(int low = arr.length - 1; low > 0; low--){
+                arr[low] = arr[low - 1];
             }
-    public static void main(String[] args) {
-        int[][] arr = {
-                {1, 2, 3, 4},
-                {5, 6, 7, 8},
-                {9, 10, 11, 12}
-        };
+            arr[0] = lastLow;
+        }
 
-        addTwoDArr(arr, 2);
-
+    public static void showTwoDArr(int[][] arr) {
         for(int i = 0; i < arr.length; i++) {
             for(int j = 0; j < arr[i].length; j++)
                 System.out.print(arr[i][j] + " ");
+
             System.out.println();
         }
     }
+
+        public static void main(String[] args) {
+            int[][] arr = {
+                    {1, 2, 3},
+                    {4, 5, 6},
+                    {7, 8, 9}
+            };
+
+            System.out.println("1st shift...");
+            shiftTwoDArr(arr);
+            showTwoDArr(arr);
+
+            System.out.println("2nd shift...");
+            shiftTwoDArr(arr);
+            showTwoDArr(arr);
+
+            System.out.println("3rd shift...");
+            shiftTwoDArr(arr);
+            showTwoDArr(arr);
+        }
 }
