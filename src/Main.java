55a87b26c8e702d5373fec3b668dcb325cc31a16
이현진 <roje2003@naver.com>
2023-04-07 11:09:43 +0900

    vargs 매개변수 가변인자

--- src/Main.java
+++ src/Main.java
@@ -1,40 +1,14 @@
-import java.util.HashSet;
-class Num {
-    private int num;
-
-    public Num(int n) {
-        num = n;
-    }
-
-    @Override
-    public String toString() {
-        return String.valueOf(num);
-    }
-
-    @Override
-    public int hashCode() {
-        return num % 3;
-    }
-
-    @Override
-    public boolean equals(Object obj) { //num의 값이 같으면 true 반환
-        if (num == ((Num) obj).num)
-            return true;
-        else
-            return false;
-    }
-}
-class HashSetEqualityTwo{
-    public static void main(String[] args){
-        HashSet<Num> set = new HashSet<>();
-        set.add(new Num(7799));
-        set.add(new Num(9955));
-        set.add(new Num(7799));
-        System.out.println("인스턴스 수: " + set.size());
-
-        for(Num n : set)
-            System.out.print(n.toString() + '\t');
+class Varargs{
+    public static void showAll(String...vargs){
+        System.out.println("LEN: " + vargs.length);
+        for(String s : vargs)
+            System.out.print(s + '\t');
         System.out.println();
+    }
+    public static void main(String[] args){
+        showAll("Box");
+        showAll("Box", "Toy");
+        showAll("Box", "Toy", "Apple");
     }
 }
 
