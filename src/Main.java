7c394b337ba87a83234124ad5cb5e8bf3a6a3cef
이현진 <roje2003@naver.com>
2023-04-10 11:22:14 +0900

    매개변수가 있고 반환하는 람다식2

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,11 @@
-interface Calculate{
-    int cal(int a, int b); // 값을 반환하는 추상 메소드
+interface HowLong{
+    int len(String s); // 값을 반환하는 메소드
 }
 
-class TwoparamAndReturn {
+class OneparamAndReturn {
     public static void main(String[] args) {
-        Calculate c;
-        c = (a, b) -> { return a + b; }; // return문의 중괄호는 생략 불가!
-        System.out.println(c.cal(4, 3));
-        c = (a, b) -> a + b; // 연산 결과가 남으면, 별도로 명시하지 않아도 반환 대상이 됨!
-        System.out.println(c.cal(4, 3));
+        HowLong h1 = s -> s.length();
+        System.out.println(h1.len("I am so happy"));
     }
 }
 
