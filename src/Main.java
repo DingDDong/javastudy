7ca5835c621925590ece7cb22d8f8721e0e2761d
이현진 <roje2003@naver.com>
2023-04-12 10:26:11 +0900

    BiFunction 예시

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,11 @@
+import java.util.function.BiFunction;
+import java.util.function.BinaryOperator;
 import java.util.function.DoubleUnaryOperator;
 import java.util.function.Function;
 class FunctionDemo{
         public static void main(String[] args) {
-                DoubleUnaryOperator f = s -> s * 0.5;
-                        System.out.println(f.applyAsDouble(5));
-                        System.out.println(f.applyAsDouble(10));
+                BiFunction<Integer, String , String> bifunction= ( num, str ) -> num + "번째 " + str ;
+                        System.out.println(bifunction.apply(3, "비행기"));
+                        System.out.println(bifunction.apply(5,"커피"));
         }
 }
