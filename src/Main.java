e3d155c308156e67e53484c5ad50355e5ff99330
이현진 <roje2003@naver.com>
2023-04-13 15:52:10 +0900

    Stream2

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,21 @@
 import java.util.Arrays;
-import java.util.stream.IntStream;
+import java.util.List;
+import java.util.function.BinaryOperator;
+
 
 class Stream{
         public static void main(String[] args) {
-                int[] ar = {1, 2, 3, 4, 5};
-                int sum = Arrays.stream(ar)
-                        .filter(n -> n % 2 == 1)
-                        .sum();
-                System.out.println(sum);
+                List<Integer> ls = Arrays.asList(5, 4, 3, 2);
+
+                BinaryOperator<Integer> lc = (s1, s2) -> {
+                        if(s1 > s2)
+                                return s1;
+                        else
+                                return s2;
+                };
+
+                Integer str = ls.stream()
+                                .reduce(0,lc);
+                System.out.println(str);
         }
 }
