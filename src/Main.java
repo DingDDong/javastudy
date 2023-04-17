b65717b4f7b0429408e9c4a56059dd1c45fe0922
이현진 <roje2003@naver.com>
2023-04-17 17:57:37 +0900

    DataOutputStream

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,15 @@
-import java.time.ZonedDateTime;
-import java.time.ZoneId;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.FileOutputStream;
 
-class ZoneIdDemo{
-        public static void main(String[] args) {
-            ZonedDateTime date = ZonedDateTime.of(LocalDateTime.of
-                    (2019, 4, 25, 11 , 20), ZoneId.of("Asia/Seoul"));
-
-            //출력의 포맷 정보는 java.time.format.DateTimeFormatter 인스턴스에 담는다.
-            DateTimeFormatter fm1 = DateTimeFormatter.ofPattern("yy-M-d");
-            DateTimeFormatter fm2 = DateTimeFormatter.ofPattern("yyyy-MM-d, H:m:s");
-            DateTimeFormatter fm3 = DateTimeFormatter.ofPattern("yyyy-MM-d, H:m:ss VV");
-            // LocalDate,LocalTime, LocalDateTime, ZonedDateTime에 모두 존재하는 format 메소드 호출한다.
-            System.out.println(date.format(fm1));
-            System.out.println(date.format(fm2));
-            System.out.println(date.format(fm3));
+class DataFilterOutputStream{
+    public static void main(String[] args){
+        try(DataOutputStream out = new DataOutputStream(new FileOutputStream("data.dat"))){
+            out.writeInt(370); // int형 데이터 저장
+            out.writeDouble(3.14); // double형 데이터 저장
         }
+        catch(IOException e){
+            e.printStackTrace();
+        }
+    }
 }
